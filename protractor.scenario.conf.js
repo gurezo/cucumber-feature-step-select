c9b5702b7523d649f953479229b6fee6e01256e2
gurezo <akihiko.kigure@gmail.com>
2018-01-22 22:33:30 +0900

    ie11 setting had modified.

--- protractor.scenario.conf.js
+++ protractor.scenario.conf.js
@@ -35,6 +35,7 @@ exports.config = {
   capabilities: {
     'browserName': 'internet explorer',
     'platform': 'ANY',
+    'ignoreProtectedModeSettings': true,
     'version': '11'
   },
   // localSeleniumStandaloneOpts: {
